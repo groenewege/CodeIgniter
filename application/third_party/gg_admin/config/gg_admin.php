33a8154379280077995fbf7cd9cd06ed295c607c
Gunther Groenewege <gunther@groenewege.com>
2011-10-23 23:37:09 +0200

    removed gg_admin package

--- application/third_party/gg_admin/config/gg_admin.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-/**
-* GG Admin Config
-*/
-	
-	$config['site_title']		   = "CodeIgniter";
-	$config['admin_email']		   = "info@g-design.net";
--- application/third_party/gg_admin/views/admin/home/index.php
+++
@@ -1,4 +0,0 @@
-<div class="hero-unit">
-	<h1><?php echo __('gg_administration'); ?></h1>
-	<p><?php echo __('gg_admin_description'); ?></p>
-</div>
