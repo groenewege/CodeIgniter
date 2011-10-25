d5763ad008501782f1716b671e50484ee305b98c
Gunther Groenewege <gunther@groenewege.com>
2011-10-25 16:31:20 +0200

    basic list for pages

--- application/models/page_gas.php
+++ application/models/page_gas.php
@@ -5,12 +5,17 @@ class Page extends Gas {
 	function _init()
     {
         $this->_fields = array(
-          'id'       	  => Gas::field('auto'),
-          'title'    	  => Gas::field('char[250]'),
-          'content'    	=> Gas::field('char'),
-          'created_at'	=> Gas::field('char'),
-          'status'   	  => Gas::field('int[1]'),
+          'id'              => Gas::field('auto'),
+          'title'           => Gas::field('char[250]'),
+          'content'         => Gas::field('char'),
+          'created_at'      => Gas::field('char'),
+          'status'          => Gas::field('int[1]'),
         );
+    }
+
+    public function get_date()
+    {
+        return date('F d Y', strtotime($this->created_at));
     }
 
 }
