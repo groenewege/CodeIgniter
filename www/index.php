fd191df03230cdd07f9c33bb46e07019737bb771
Gunther Groenewege <gunther@groenewege.com>
2012-03-04 01:48:07 +0100

    automatic environment, changed gg_admin remote

--- www/index.php
+++ www/index.php
@@ -18,7 +18,11 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+	if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+		define('ENVIRONMENT', 'development');
+	} else {
+		define('ENVIRONMENT', 'production');	
+	}
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
